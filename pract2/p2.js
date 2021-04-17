c79642b427a4d2c5b1ea02394da784e486620323
guepardito <guepardito043@gmail.com>
2021-04-17 10:05:51 +0200

    intento de arreglo de bugs en p2

--- pract2/p2.js
+++ pract2/p2.js
@@ -2,6 +2,7 @@ function Sumar() {
     var n1 = document.getElementById('n1').value;
     var n2 = document.getElementById('n2').value;
     var n3 = document.getElementById('n3').value;
-    var suma = parseFloat(n1) * parseFloat(n2) * parseFloat(n3);
-    alert("La suma es: "+suma)
+
+    var multiplicacion = parseFloat(n1) * parseFloat(n2) * parseFloat(n3);
+    alert("El total es: "+multiplicacion)
 }
--- pract3/p3.js
+++ pract3/p3.js
@@ -1,6 +1,6 @@
 function Sumar() {
     var base = document.getElementById('base').value;
     var altura = document.getElementById('altura').value;
-    var suma = (parseFloat(base) * parseFloat(altura)) / 2;
-    alert("La suma es: "+suma)
+    var area = (parseFloat(base) * parseFloat(altura)) / 2;
+    alert("El area es: "+area)
 }
